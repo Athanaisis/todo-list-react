2e509d681063faa58fd3b36387c8aa33a0b76904
Anasthasia LAVRIL <76066385+Athanaisis@users.noreply.github.com>
2022-05-17 14:38:18 +0200

    Projet react

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// On importe la librairie react
+import React from 'react'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// On importe la librairie react-dom (elle permet)
+// d'afficher react sur la page
+import { createRoot } from 'react-dom/client'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+type HelloPropos = {
+  nom: string
+  age?: string
+}
+
+function Hello({ nom, age = "pas d'âge" }: HelloPropos): JSX.Element {
+  return (
+    <p>
+      Hello {nom}, vous avez {age} ans :)
+    </p>
+  )
+}
+
+//On crée un élément react (JSX)
+const element = (
+  <div>
+    <h1> Bonjour la terre !</h1>
+    <h2>Comment allez-vous les amis ?!</h2>
+    <Hello nom="Rose" />
+    <Hello nom="Jérémy" age="54" />
+    <Hello nom="Stéphanie" age="68" />
+  </div>
+)
+
+//Onrécupère la balise div#root de notre
+// page HTML
+const root = document.querySelector('#root')
+
+//S'il n'y pas de balise root
+if (!root) {
+  //On lève une erreur
+  throw new Error('Oups... il manque la balise root dans votre div')
+}
+createRoot(root).render(element)
